d4f8cc87f78a6b2d076f413c0bbbf1aa5f0ddf80
Viktar Rudzenia <viktarrudzenia@gmail.com>
2024-09-07 17:22:24 +0200

    feat: added cypress for e2e tests and one e2e test

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,10 +8,10 @@ import styles from './not-found.module.scss';
 export default function NotFound() {
   return (
     <div className={styles.wrapper}>
-      <h2>Page not found...</h2>
+      <h2 data-testid="page-not-found">Page not found...</h2>
       <p>We&apos;re unable to find the page you&apos;re looking for</p>
       <FoButton>
-        <Link className={styles.link} href={AllRoutesEnum.HOME}>
+        <Link data-testid="back-to-home" className={styles.link} href={AllRoutesEnum.HOME}>
           Back to Home
         </Link>
       </FoButton>
